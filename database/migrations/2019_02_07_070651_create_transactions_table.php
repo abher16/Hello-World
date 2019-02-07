3571f6cc44b1f96573c9fdf9c6d702f8c223580d
abher16 <47419852+abher16@users.noreply.github.com>
2019-02-07 20:40:28 +0700

    Add transaction table migration

---
+++ database/migrations/2019_02_07_070651_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('item', 100);
+            $table->integer('count');
+            $table->integer('price');
+            $table->integer('paid');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
